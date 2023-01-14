cecd063a947e59d722eea051f91d38fdc07a1f3f
waiyanhein <iljimae.ic@gmail.com>
2023-01-14 07:15:45 +0000

    unit tests for errorResponse

--- packages/core/tests/unit/utilities/response.spec.ts
+++ packages/core/tests/unit/utilities/response.spec.ts
@@ -1,7 +1,11 @@
 import { Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { faker } from '@faker-js/faker';
-import { successResponse } from '../../../src/utilities/response';
+import {
+  successResponse,
+  errorResponse
+} from '../../../src/utilities/response';
+import { number } from 'joi';
 
 class FakeExpressResponse {
   status = (status: number) => {
@@ -19,18 +23,62 @@ describe(`Response Utility`, () => {
       };
       successResponse(resMock, data);
 
-      expect((resMock.status as jest.Mock).mock.calls.length).toBe(1);
-      const statusCall = (resMock.status as jest.Mock).mock.calls[0];
-      expect(statusCall[0]).toBe(StatusCodes.OK);
-      expect((resMock.json as jest.Mock).mock.calls.length).toBe(1);
-      const jsonCall = (resMock.json as jest.Mock).mock.calls[0];
-      expect(JSON.stringify(jsonCall[0])).toBe(
-        JSON.stringify({
-          data
-        })
-      );
+      assertResponseStatus(resMock, StatusCodes.OK);
+      assertResponseJson(resMock, {
+        data
+      });
     });
   });
+
+  describe(`errorResponse`, () => {
+    it(`should return response in the correct structure`, () => {
+      const resMock = mockExpressResponse();
+      const error = {
+        message: faker.lorem.sentence(4),
+        code: `UnknownException`,
+        details: {
+          requestId: faker.datatype.uuid(),
+          traceId: faker.datatype.uuid()
+        }
+      };
+      errorResponse(resMock, error, StatusCodes.INTERNAL_SERVER_ERROR);
+      assertResponseStatus(resMock, StatusCodes.INTERNAL_SERVER_ERROR);
+      assertResponseJson(resMock, {
+        error
+      });
+    });
+
+    it(`should return error message string in the correct structure`, () => {
+      const resMock = mockExpressResponse();
+      errorResponse(resMock, `Unauthorised`, StatusCodes.UNAUTHORIZED);
+      assertResponseStatus(resMock, StatusCodes.UNAUTHORIZED);
+      assertResponseJson(resMock, {
+        error: {
+          message: `Unauthorised`
+        }
+      });
+    });
+  });
+
+  const assertResponseStatus = (
+    resMock: Response,
+    expectedStatusCode: number
+  ) => {
+    expect((resMock.status as jest.Mock).mock.calls.length).toBe(1);
+    const statusCall = (resMock.status as jest.Mock).mock.calls[0];
+    expect(statusCall[0]).toBe(expectedStatusCode);
+  };
+
+  const assertResponseJson = <T>(
+    resMock: Response,
+    expectedResponseData: T
+  ) => {
+    expect((resMock.json as jest.Mock).mock.calls.length).toBe(1);
+    const jsonCall = (resMock.json as jest.Mock).mock.calls[0];
+    expect(JSON.stringify(jsonCall[0])).toBe(
+      JSON.stringify(expectedResponseData)
+    );
+  };
 
   const mockExpressResponse = (): Response => {
     const jsonMockFunc = jest.fn().mockImplementation((data) => data);
