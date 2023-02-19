75932bad95de3a58593b52c9eb5630fcb63eea57
waiyanhein <iljimae.ic@gmail.com>
2023-02-19 14:52:55 +0000

    question comment - config -> baseConfig?

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,6 +1,7 @@
 import { BaseConfig } from './types/baseConfig';
 import { Config } from './config';
 
+// TODO: rename this to baseConfig?
 export const config: BaseConfig = Config.getConfig();
 
 export * from './types';
