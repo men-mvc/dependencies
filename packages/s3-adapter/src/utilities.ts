c062f092d2b3f6778c20ca736ced11e493f91376
waiyanhein <iljimae.ic@gmail.com>
2023-03-12 00:55:20 +0000

    remove s3 credentials

--- packages/s3-adapter/src/utilities.ts
+++ packages/s3-adapter/src/utilities.ts
@@ -1,16 +1,9 @@
 import { baseConfig } from '@men-mvc/config';
 
-// export const getAwsS3Bucket = (): string => baseConfig.fileSystem.s3?.bucket ?? ``;
-export const getAwsS3Bucket = (): string => `men-mvc-local`;
+export const getAwsS3Bucket = (): string => baseConfig.fileSystem.s3?.bucket ?? ``;
 
 export const getAwsS3Credentials = () => ({
-  region: `eu-west-2`,
-  accessKeyId: `AKIAYKM4VUKOA7X64F5W`,
-  secretAccessKey: `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`
+  region: baseConfig.fileSystem?.s3?.region,
+  accessKeyId: baseConfig.fileSystem?.s3?.accessKeyId??``,
+  secretAccessKey: baseConfig.fileSystem?.s3?.secretAccessKey??``
 });
-
-// export const getAwsS3Credentials = () => ({
-//   region: baseConfig.fileSystem?.s3?.region,
-//   accessKeyId: baseConfig.fileSystem?.s3?.accessKeyId??``,
-//   secretAccessKey: baseConfig.fileSystem?.s3?.secretAccessKey??``
-// });
