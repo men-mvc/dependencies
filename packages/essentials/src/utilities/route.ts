62eebd980deb9393f5b385ee4caf2dd70f398248
waiyanhein <iljimae.ic@gmail.com>
2023-04-07 21:38:27 +0100

    replaceRouteParams function

---
+++ packages/essentials/src/utilities/route.ts
@@ -0,0 +1,17 @@
+export const replaceRouteParams = (
+  route: string,
+  params: Record<string, string>
+): string => {
+  if (!route) {
+    return route;
+  }
+
+  const paramKeys = Object.keys(params);
+  let newRoute = route;
+
+  for (const key of paramKeys) {
+    newRoute = newRoute.replace(`:${key}`, params[key]);
+  }
+
+  return newRoute.startsWith('/') ? newRoute : `/${newRoute}`;
+};
