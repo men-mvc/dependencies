de28f84aef9442b58054bd06c4c31ad3f5ad6e0d
waiyanhein <iljimae.ic@gmail.com>
2023-05-07 07:39:37 +0100

    cloudwatch logger

--- packages/config/src/types/loggerConfig.ts
+++ packages/config/src/types/loggerConfig.ts
@@ -7,4 +7,11 @@ export type LoggerConfig = {
     dsn: string;
     tracesSampleRate?: number;
   };
+  cloudwatch?: {
+    logGroupName: string;
+    region: string;
+    accessKeyId: string;
+    secretAccessKey: string;
+    logStreamPrefix?: string;
+  }
 };
